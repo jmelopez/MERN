65c813c3b3d874723d71b3bead58453aff4f5395
jmelopez <jmelopez@outlook.com>
2022-11-10 18:08:09 -0700

    completed assignment

---
+++ react/intro/first-react-jsx/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Practice Java & Python</li>
+        <li>Pay bills</li>
+        <li>Pack bag for upcoming trip</li>
+        <li>Play game!</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
